1b162783b12cc068620fa77fd81007941705c9fc
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-01-10 15:22:40 +0900

    master third commit

--- test1.py
+++ test1.py
@@ -47,8 +47,8 @@ while running:
                 print("6 눌림")
             if event.key == pygame.K_8:
                 print("8 눌림 (아마)")
-            if event.key == pygame.K_9:
-                print("언제까지 하는 거냐 이거")
+            if event.key == pygame.K_6:
+                print("으헹흐헝")
              
          
         if event.type == pygame.KEYUP:
